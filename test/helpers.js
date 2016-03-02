dcb7fcd6f58bf44457056a37aade0b0b44ac52ec
Scott Dixon <scott@howrefreshing.com.au>
2016-03-02 15:43:09 -0800

    Windows tests: append .sh or .bat where required

--- test/helpers.js
+++ test/helpers.js
@@ -1,8 +1,12 @@
 import path from 'path';
+import { system } from 'appium-support';
 
 function getFixture (fix) {
+  // Append .bat or .sh is there's no extention
+  if (fix.indexOf('.') === -1) {
+    fix = fix + (system.isWindows() ? '.bat' : '.sh');
+  }
   return path.resolve(__dirname, "..", "..", "test", "fixtures", fix);
 }
 
-export { getFixture };
-
+export { getFixture, system };
