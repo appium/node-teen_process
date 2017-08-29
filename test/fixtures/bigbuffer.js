e8ba418695ef2f66567e27d453d2077cf1267230
Isaac Murchie <isaac@saucelabs.com>
2017-08-29 16:09:16 -0400

    Add pre-commit hooks

--- test/fixtures/bigbuffer.js
+++ test/fixtures/bigbuffer.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env node
+/* eslint-disable no-var */
 
 const MAX_BUFFER_BYTES = 512 * 1024;
 const chars = parseInt(MAX_BUFFER_BYTES * 1.5, 10);
 const asciiRange = [65, 110];
 
-let ch;
-for (let i = 0; i < chars; i++) {
+var ch;
+for (var i = 0; i < chars; i++) {
   ch = (i % (asciiRange[1] - asciiRange[0])) + asciiRange[0];
   process.stdout.write(String.fromCharCode(ch));
 }
