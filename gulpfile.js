ac6495e172623c934c762f8121e9f81999f32148
Isaac A. Murchie <isaac.appium@gmail.com>
2017-08-30 15:59:44 -0400

    Update linting (#18)
    * Update linting
    
    * Add pre-commit hooks
    
    * Add pre-commit hooks
    
    * Update test to test correctness

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,6 @@
 "use strict";
 
-var gulp = require('gulp'),
-    boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp);
+const gulp = require('gulp');
+const boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp);
 
-boilerplate({build: 'teen_process', jscs: false});
+boilerplate({build: 'teen_process'});
