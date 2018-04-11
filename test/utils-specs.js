0b173e489867068e6658e788bd3732a92f274049
Mykola Mokhnach <nick+1@wire.com>
2018-04-11 19:55:19 +0200

    Address review comments

--- test/utils-specs.js
+++ test/utils-specs.js
@@ -1,37 +1,37 @@
 // transpile:mocha
 
-import { quoteSpawnArgument } from '../lib/utils';
+import { quoteSpawnArguments } from '../lib/utils';
 import chai from 'chai';
 import chaiAsPromised from 'chai-as-promised';
 import { quote } from 'shell-quote';
 
-const should = chai.should();
 chai.use(chaiAsPromised);
 
 describe('utils', function () {
   describe('quoteSpawnArgument', function () {
-    it('should properly quote arguments with non-literal chars', function () {
-      const args = ['arg_without_spaces', 'arg with spaces'];
-      const quotedArgs = args.map(arg => quoteSpawnArgument(arg));
+    it('should properly quote arguments with whitespace chars', function () {
+      const quotedArgs = quoteSpawnArguments(['arg_without_spaces', 'arg with spaces']);
       quotedArgs.should.eql(['arg_without_spaces', quote(['arg with spaces'])]);
     });
 
-    it('should skip arguments quoted with double quote char', function () {
-      const args = ['arg_without_spaces', '"quoted arg with spaces"'];
-      const quotedArgs = args.map(arg => quoteSpawnArgument(arg));
+    it('should skip arguments quoted with a double quote char', function () {
+      const quotedArgs = quoteSpawnArguments(['arg_without_spaces', '"quoted arg with spaces"']);
       quotedArgs.should.eql(['arg_without_spaces', '"quoted arg with spaces"']);
     });
 
-    it('should skip arguments quoted with single quote char', function () {
-      const args = ['arg_without_spaces', '\'quoted arg with spaces\''];
-      const quotedArgs = args.map(arg => quoteSpawnArgument(arg));
+    it('should skip arguments quoted with a single quote char', function () {
+      const quotedArgs = quoteSpawnArguments(['arg_without_spaces', '\'quoted arg with spaces\'']);
       quotedArgs.should.eql(['arg_without_spaces', '\'quoted arg with spaces\'']);
     });
 
     it('should skip invalid arguments', function () {
-      const args = ['arg_without_spaces', null];
-      const quotedArgs = args.map(arg => quoteSpawnArgument(arg));
+      const quotedArgs = quoteSpawnArguments(['arg_without_spaces', null]);
       quotedArgs.should.eql(['arg_without_spaces', null]);
+    });
+
+    it('should work with a single non-array argument', function () {
+      const quotedArgs = quoteSpawnArguments('arg_without_spaces');
+      quotedArgs.should.eql(['arg_without_spaces']);
     });
   });
 });
