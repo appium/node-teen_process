19822f09c6a89ff552a1fa35a982e8250e025fb7
Jonathan Lipps <jlipps@gmail.com>
2018-10-13 15:50:52 -0700

    use typescript instead of babel and port exec to typescript

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,10 @@
 "use strict";
 
 const gulp = require('gulp');
+const config = require('./tsconfig.json').compilerOptions;
 const boilerplate = require('appium-gulp-plugins').boilerplate.use(gulp);
 
-boilerplate({build: 'teen_process'});
+// make sure we use typescript local to this package, not gulp plugins package
+config.typescript = require('typescript');
+
+boilerplate({build: 'teen_process', tsNotBabel: true, tsConfig: config});
--- lib/exec.ts
+++ lib/exec.ts
@@ -1,55 +1,88 @@
-/* eslint-disable promise/prefer-await-to-callbacks */
-
 import { spawn } from 'child_process';
 import { quote } from 'shell-quote';
 import B from 'bluebird';
 
+interface ExecOpts {
+  cwd?: string,
+  env?: NodeJS.ProcessEnv,
+  shell?: string
+  timeout?: number,
+  encoding?: string,
+  killSignal?: string,
+  ignoreOutput?: boolean,
+  stdio?: string[] | number[] | string,
+  isBuffer?: boolean
+}
 
-function exec (cmd, args = [], opts = {}) {
+interface ExecResponse {
+  stdout: string | Buffer,
+  stderr: string | Buffer,
+  code: number,
+}
+
+class ExecError extends Error implements ExecResponse {
+  stdout: string | Buffer = null;
+  stderr: string | Buffer = null;
+  code: number = null;
+
+  constructor (message: string) {
+    super(message);
+  }
+}
+
+const defaultExecOpts: ExecOpts = {
+  timeout: null,
+  encoding: 'utf8',
+  killSignal: 'SIGTERM',
+  cwd: undefined,
+  env: process.env,
+  ignoreOutput: false,
+  stdio: "inherit",
+  isBuffer: false,
+  shell: undefined,
+};
+
+function exec (cmd: string, args: string[] = [], opts: ExecOpts = defaultExecOpts) {
   // get a quoted representation of the command for error strings
   const rep = quote([cmd, ...args]);
 
   // extend default options; we're basically re-implementing exec's options
   // for use here with spawn under the hood
-  opts = Object.assign({
-    timeout: null,
-    encoding: 'utf8',
-    killSignal: 'SIGTERM',
-    cwd: undefined,
-    env: process.env,
-    ignoreOutput: false,
-    stdio: "inherit",
-    isBuffer: false,
-    shell: undefined,
-  }, opts);
+  opts = {
+    ...defaultExecOpts,
+    ...opts
+  };
 
   // this is an async function, so return a promise
-  return new B((resolve, reject) => {
+  
+  return new B<ExecResponse>((resolve, reject) => {
     // spawn the child process with options; we don't currently expose any of
     // the other 'spawn' options through the API
     let proc = spawn(cmd, args, {cwd: opts.cwd, env: opts.env, shell: opts.shell});
-    let stdoutArr = [], stderrArr = [], timer = null;
+    let stdoutArr: Buffer[] = [];
+    let stderrArr: Buffer[] = [];
+    let timer: NodeJS.Timer = null;
 
     // if the process errors out, reject the promise
-    proc.on('error', (err) => {
+    proc.on('error', (err: NodeJS.ErrnoException) => {
       let msg = `Command '${rep}' errored out: ${err.stack}`;
-      if (err.errno === 'ENOENT') {
+      if (err.code === 'ENOENT') {
         msg = `Command '${cmd}' not found. Is it installed?`;
       }
       reject(new Error(msg));
     });
     if (proc.stdin) {
-      proc.stdin.on('error', (err) => {
+      proc.stdin.on('error', (err: NodeJS.ErrnoException) => {
         reject(new Error(`Standard input '${err.syscall}' error: ${err.stack}`));
       });
     }
     if (proc.stdout) {
-      proc.stdout.on('error', (err) => {
+      proc.stdout.on('error', (err: NodeJS.ErrnoException) => {
         reject(new Error(`Standard output '${err.syscall}' error: ${err.stack}`));
       });
     }
     if (proc.stderr) {
-      proc.stderr.on('error', (err) => {
+      proc.stderr.on('error', (err: NodeJS.ErrnoException) => {
         reject(new Error(`Standard error '${err.syscall}' error: ${err.stack}`));
       });
     }
@@ -57,18 +90,18 @@ function exec (cmd, args = [], opts = {}) {
     // keep track of stdout/stderr if we haven't said not to
     if (!opts.ignoreOutput) {
       if (proc.stdout) {
-        proc.stdout.on('data', (data) => {
+        proc.stdout.on('data', (data: Buffer) => {
           stdoutArr.push(data);
         });
       }
       if (proc.stderr) {
-        proc.stderr.on('data', (data) => {
+        proc.stderr.on('data', (data: Buffer) => {
           stderrArr.push(data);
         });
       }
     }
 
-    function getStdio (isBuffer) {
+    function getStdio (isBuffer: boolean) {
       let stdout, stderr;
       if (isBuffer) {
         stdout = Buffer.concat(stdoutArr);
@@ -88,11 +121,12 @@ function exec (cmd, args = [], opts = {}) {
         clearTimeout(timer);
       }
       let {stdout, stderr} = getStdio(opts.isBuffer);
+      let response: ExecResponse = {stdout, stderr, code};
       if (code === 0) {
-        resolve({stdout, stderr, code});
+        resolve(response);
       } else {
         let err = new Error(`Command '${rep}' exited with code ${code}`);
-        err = Object.assign(err, {stdout, stderr, code});
+        err = {...err, ...response};
         reject(err);
       }
     });
@@ -103,8 +137,8 @@ function exec (cmd, args = [], opts = {}) {
     if (opts.timeout) {
       timer = setTimeout(() => {
         let {stdout, stderr} = getStdio(opts.isBuffer);
-        let err = new Error(`Command '${rep}' timed out after ${opts.timeout}ms`);
-        err = Object.assign(err, {stdout, stderr, code: null});
+        let err = new ExecError(`Command '${rep}' timed out after ${opts.timeout}ms`);
+        err.stdout = stdout, err.stderr = stderr, err.code = null;
         reject(err);
         // reject and THEN kill to avoid race conditions with the handlers
         // above
