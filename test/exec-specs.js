d733c8943bcc9b2abb45265003c8b1cddbef2749
Isaac A. Murchie <isaac.appium@gmail.com>
2019-01-09 09:06:21 -0500

    Update linting (#45)
    * Update linting
    
    * Remove appium-support from deps

--- test/exec-specs.js
+++ test/exec-specs.js
@@ -17,8 +17,8 @@ describe('exec', function () {
     let cmd = 'ls';
     let args = [__dirname];
     let {stdout, stderr, code} = await exec(cmd, args);
-    stdout.should.contain("exec-specs.js");
-    stderr.should.equal("");
+    stdout.should.contain('exec-specs.js');
+    stderr.should.equal('');
     code.should.equal(0);
   });
 
@@ -35,7 +35,7 @@ describe('exec', function () {
   });
 
   it('should throw an error with a bad exit code', async function () {
-    let cmd = getFixture("bad_exit");
+    let cmd = getFixture('bad_exit');
     let err;
     try {
       await exec(cmd);
@@ -43,15 +43,15 @@ describe('exec', function () {
       err = e;
     }
     should.exist(err);
-    err.stdout.trim().should.equal("foo");
-    err.stderr.trim().should.equal("bar");
+    err.stdout.trim().should.equal('foo');
+    err.stderr.trim().should.equal('bar');
     err.code.should.equal(1);
   });
 
   it('should work with spaces in arguments', async function () {
-    let cmd = getFixture("echo");
-    let echo1 = "my name is bob";
-    let echo2 = "lol";
+    let cmd = getFixture('echo');
+    let echo1 = 'my name is bob';
+    let echo2 = 'lol';
     let {stdout, stderr, code} = await exec(cmd, [echo1, echo2]);
     stdout.trim().should.equal(echo1);
     stderr.trim().should.equal(echo2);
@@ -59,9 +59,9 @@ describe('exec', function () {
   });
 
   it('should work with backslashes in arguments', async function () {
-    let cmd = getFixture("echo");
-    let echo1 = "my\\ name\\ is\\ bob";
-    let echo2 = "lol";
+    let cmd = getFixture('echo');
+    let echo1 = 'my\\ name\\ is\\ bob';
+    let echo2 = 'lol';
     let {stdout, stderr, code} = await exec(cmd, [echo1, echo2]);
     stdout.trim().should.equal(echo1);
     stderr.trim().should.equal(echo2);
@@ -69,9 +69,9 @@ describe('exec', function () {
   });
 
   it('should work with spaces in commands', async function () {
-    let cmd = getFixture("echo with space");
-    let echo1 = "bobbob";
-    let echo2 = "lol";
+    let cmd = getFixture('echo with space');
+    let echo1 = 'bobbob';
+    let echo2 = 'lol';
     let {stdout, stderr, code} = await exec(cmd, [echo1, echo2]);
     stdout.trim().should.equal(echo1);
     stderr.trim().should.equal(echo2);
@@ -79,9 +79,9 @@ describe('exec', function () {
   });
 
   it('should work with spaces in commands and arguments', async function () {
-    let cmd = getFixture("echo with space");
-    let echo1 = "my name is bob";
-    let echo2 = "lol";
+    let cmd = getFixture('echo with space');
+    let echo1 = 'my name is bob';
+    let echo2 = 'lol';
     let {stdout, stderr, code} = await exec(cmd, [echo1, echo2]);
     stdout.trim().should.equal(echo1);
     stderr.trim().should.equal(echo2);
@@ -89,10 +89,10 @@ describe('exec', function () {
   });
 
   it('should respect cwd', async function () {
-    let cmd = system.isWindows() ? "echo.bat" : "./echo.sh";
-    let echo1 = "my name is bob";
-    let echo2 = "lol";
-    let cwd = path.dirname(getFixture("echo"));
+    let cmd = system.isWindows() ? 'echo.bat' : './echo.sh';
+    let echo1 = 'my name is bob';
+    let echo2 = 'lol';
+    let cwd = path.dirname(getFixture('echo'));
     let {stdout, stderr, code} = await exec(cmd, [echo1, echo2], {cwd});
     stdout.trim().should.equal(echo1);
     stderr.trim().should.equal(echo2);
@@ -100,16 +100,16 @@ describe('exec', function () {
   });
 
   it('should respect env', async function () {
-    let cmd = getFixture("env");
-    let env = {FOO: "lolol"};
+    let cmd = getFixture('env');
+    let env = {FOO: 'lolol'};
     let {stdout, code} = await exec(cmd, [], {env});
     stdout.trim().should.equal(`${env.FOO} ${env.FOO}`);
     code.should.equal(0);
   });
 
   it('should allow a timeout parameter', async function () {
-    let cmd = "sleep";
-    let args = ["10"];
+    let cmd = 'sleep';
+    let args = ['10'];
     let err;
     try {
       await exec(cmd, args, {timeout: 500});
@@ -117,27 +117,27 @@ describe('exec', function () {
       err = e;
     }
     should.exist(err);
-    err.message.should.contain("timed out");
+    err.message.should.contain('timed out');
     err.message.should.contain(cmd);
   });
 
   it('should allow large amounts of output', async function () {
     this.timeout(24000);
-    let {stdout} = await exec(getFixture("bigbuffer.js"));
+    let {stdout} = await exec(getFixture('bigbuffer.js'));
     stdout.length.should.be.above(512 * 1024);
   });
 
   it('should ignore output if requested', async function () {
-    let cmd = getFixture("echo.sh");
-    let echo1 = "my name is bob";
+    let cmd = getFixture('echo.sh');
+    let echo1 = 'my name is bob';
     let {stdout, code} = await exec(cmd, [echo1], {ignoreOutput: true});
-    stdout.should.equal("");
+    stdout.should.equal('');
     code.should.equal(0);
   });
 
   it('should return a Buffer if requested', async function () {
-    let cmd = getFixture("echo.sh");
-    let echo1 = "my name is bob";
+    let cmd = getFixture('echo.sh');
+    let echo1 = 'my name is bob';
     let {stdout, stderr, code} = await exec(cmd, [echo1], {isBuffer: true});
     _.isString(stdout).should.be.false;
     _.isBuffer(stdout).should.be.true;
